bbe99ddf23a8ef825d42f6c9c069268145a9cc82
江济溪 <398670954@qq.com>
2023-06-04 10:24:26 +0800

    feat:新增自定义导航栏

--- app.js
+++ app.js
@@ -1,21 +1,16 @@
 import localConfigs from './config'
 App({
   onLaunch() {
-    // 展示本地存储能力
-    const logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
-
-    // 登录
-    wx.login({
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
+    wx.getSystemInfo({
+      success: e => {
+        this.globalData.StatusBar = e.statusBarHeight;
+        let custom = wx.getMenuButtonBoundingClientRect();
+        this.globalData.Custom = custom;
+        this.globalData.CustomBar = custom.bottom + custom.top - e.statusBarHeight;
       }
     })
   },
-  globalData: {
-    userInfo: null
-  },
+  globalData: {},
 
   // 获取配置
   getConfig(key = "") {
